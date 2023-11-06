5a995c18c68ccf6d2bdfc263b7ec49324c334d1c
Koray Afiyet <korayafiyet.bsns@gmail.com>
2023-11-06 03:44:44 +0300

    Update MainHeaderMobil.jsx

--- src/components/MainHeaderMobil.jsx
+++ src/components/MainHeaderMobil.jsx
@@ -7,14 +7,9 @@ import { useSelector } from 'react-redux';
 const MainHeaderMobil = () => {
     const acilis = useSelector((state) => state.open.value)
     return (
-
-
         <div className={`z-50 flex justify-start items-end ${acilis?"h-[20%]":"h-full"}  w-full bg-white`}>
-            
             <div className={` ${acilis ? "h-full" : "h-full"} w-full flex justify-center items-center `}>
-            
                 <div className='sec1 flex justify-center items-center h-full w-full'>
-
                     <div className='sec11 p-8 flex justify-center items-center h-full w-full'>
                         <div className='p-2 border-r-[0.07rem] border-stone-700'>
                             <AiOutlineInstagram className='text-stone-700 w-[1.5rem] h-[1.5rem] ' />
@@ -26,17 +21,11 @@ const MainHeaderMobil = () => {
                             <BiLogoFacebook className='text-stone-700 w-[1.5rem] h-[1.5rem] ' />
                         </div>
                     </div>
-
-
                 </div>
                 <div className='sec2 flex justify-center items-center h-full w-full max-2xl:hidden'>
-
                     {/* bbbbbbbbbbbbbbbbb */}
                 </div>
-
-
                 <div className='sec3 flex justify-center items-center h-full w-full'>
-
                     <div className='sec32 p-8  flex justify-center items-center h-full w-full '>
                         <div className='p-2'>
                             <BiSolidPhoneCall className='text-stone-700 w-[1.5rem] h-[1.5rem]' />
@@ -46,10 +35,7 @@ const MainHeaderMobil = () => {
                         </div>
                     </div>
                 </div>
-
-
             </div>
-
         </div>
 
 
