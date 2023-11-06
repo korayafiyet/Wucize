44fa5687cde0090bc6c9878a34a2dbbef5c8aac3
Koray Afiyet <korayafiyet.bsns@gmail.com>
2023-11-06 03:45:05 +0300

    Update MainBody.jsx

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -2,20 +2,12 @@ import React from 'react'
 import Surgery from './compincomp/Surgery'
 import Surgerymeme from './compincomp/Surgerymeme'
 import Surgeryvucut from './compincomp/Surgeryvucut'
-import surg1 from '../stores/stores/surg1'
-import surg2 from '../stores/stores/surg2'
-import surg3 from '../stores/stores/surg3'
 import { true1 } from '../stores/stores/surg1'
 import { true2 } from '../stores/stores/surg2'
 import { true3 } from '../stores/stores/surg3'
-
 import { false1 } from '../stores/stores/surg1'
 import { false2 } from '../stores/stores/surg2'
 import { false3 } from '../stores/stores/surg3'
-
-import tts1 from '../stores/stores/tts1'
-import tts2 from '../stores/stores/tts2'
-import tts3 from '../stores/stores/tts3'
 import { ttstrue1 } from '../stores/stores/tts1'
 import { ttstrue2 } from '../stores/stores/tts2'
 import { ttstrue3 } from '../stores/stores/tts3'
@@ -23,8 +15,6 @@ import { ttsfalse1 } from '../stores/stores/tts1'
 import { ttsfalse2 } from '../stores/stores/tts2'
 import { ttsfalse3 } from '../stores/stores/tts3'
 import { useState } from 'react'
-
-
 import { UseSelector, useDispatch, useSelector } from 'react-redux'
 
 const MainBody = () => {
@@ -35,8 +25,6 @@ const MainBody = () => {
     const althizmet1 = useSelector((state) => state.tts1.value)
     const althizmet2 = useSelector((state) => state.tts2.value)
     const althizmet3 = useSelector((state) => state.tts3.value)
-
-
     const [x, setX] = useState("visible");
     const [y, setY] = useState("hidden");
     const [z, setZ] = useState("hidden");
@@ -47,7 +35,6 @@ const MainBody = () => {
         setX(x => "opacity-100")
         setY(y => "opacity-0 absolute")
         setZ(z => "opacity-0 absolute")
-
     }
     const meme = () => {
         dispatch(true2())
@@ -56,7 +43,6 @@ const MainBody = () => {
         setX(x => "opacity-0 absolute")
         setY(y => "opacity-100")
         setZ(z => "opacity-0 absolute")
-
     }
     const vücut = () => {
         dispatch(true3())
@@ -66,22 +52,10 @@ const MainBody = () => {
         setY(y => "opacity-0 absolute")
         setZ(z => "opacity-100")
     }
-
-
-
-
-
-
-
-
-
     const [a, setA] = useState(1);
     const [b, setB] = useState(2);
     const [c, setC] = useState(3);
     const [yol, setYol] = useState(0)
-
-
-
     const tts_bir = () => {
         dispatch(ttstrue1())
         dispatch(ttsfalse2())
@@ -90,18 +64,15 @@ const MainBody = () => {
         setB(b => 2)
         setC(c => 3)
         setYol(yol => 1)
-
     }
     const tts_iki = () => {
         dispatch(ttstrue2())
         dispatch(ttsfalse1())
         dispatch(ttsfalse3())
-
         setA(a => 4)
         setB(b => 5)
         setC(c => 6)
         setYol(yol => 2)
-
     }
     const tts_uc = () => {
         dispatch(ttstrue3())
@@ -111,17 +82,12 @@ const MainBody = () => {
         setB(b => 8)
         setC(c => 9)
         setYol(yol => 3)
-
-
     }
-
-
     return (
-
         <div className='flex justify-center items-center h-full w-full bg-üstgri '>
             <div className='flex flex-col justify-center items-center h-full w-full container'>
                 <div className='flex justify-start items-center h-full w-full'>
-                    <p className='font-hizmet text-stone-700 text-[2rem] border-b-[0.1rem] border-karelaci'>Hizmetlerimiz</p>
+                    <h3 className='font-hizmet text-stone-700 text-[2rem] border-b-[0.1rem] border-karelaci'>Hizmetlerimiz</h3>
                 </div>
                 <div className='flex justify-center items-center h-full w-full my-[2rem]'>
                     <ul className='font-bold text-stone-700 flex justify-around items-center h-full w-[30%] '>
@@ -135,9 +101,6 @@ const MainBody = () => {
                     <div className={`${x} duration-1000`}><Surgery sayi1={a} sayi2={b} sayi3={c} yol={yol} /></div>
                     <div className={`${y} duration-1000`}><Surgerymeme sayi1={a} sayi2={b} sayi3={c} yol={yol} /></div>
                     <div className={`${z} duration-1000`}><Surgeryvucut sayi1={a} sayi2={b} sayi3={c} yol={yol} /></div>
-
-
-
                 </div>
                 {/*  */}
                 <div className='flex justify-center items-center h-full w-full my-[2rem]'>
@@ -145,7 +108,6 @@ const MainBody = () => {
                         <li onClick={tts_bir} className={` cursor-pointer flex justify-center items-center w-[0.7rem] h-[0.7rem] rounded-full duration-500 bg-karelaci ${althizmet1 ? "border-[0.1rem] border-black" : ""}`}></li>
                         <li onClick={tts_iki} className={`cursor-pointer flex justify-center items-center w-[0.7rem] h-[0.7rem] rounded-full duration-500 bg-karelaci ${althizmet2 ? "border-[0.1rem] border-black" : ""}`}></li>
                         <li onClick={tts_uc} className={`cursor-pointer flex justify-center items-center w-[0.7rem] h-[0.7rem] rounded-full duration-500 bg-karelaci ${althizmet3 ? "border-[0.1rem] border-black" : ""}`}></li>
-
                     </ul>
                 </div>
             </div>
