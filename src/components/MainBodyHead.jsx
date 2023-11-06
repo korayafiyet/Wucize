f2ede7bbc906112a48c1da2b53e95f05751d151e
Koray Afiyet <korayafiyet.bsns@gmail.com>
2023-11-06 03:45:02 +0300

    Update MainBodyHead.jsx

--- src/components/MainBodyHead.jsx
+++ src/components/MainBodyHead.jsx
@@ -15,37 +15,24 @@ const MainBodyHead = () => {
     const wpkay1 = useSelector((state) => state.wp1.value)
     const wpkay2 = useSelector((state) => state.wp2.value)
     const wpkay3 = useSelector((state) => state.wp3.value)
-
-
     const say1 = () => {
         dispatch(wptrue1())
         dispatch(wpfalse2())
         dispatch(wpfalse3())
-
-
     }
     const say2 = () => {
         dispatch(wptrue2())
         dispatch(wpfalse1())
         dispatch(wpfalse3())
-
-
     }
     const say3 = () => {
         dispatch(wptrue3())
         dispatch(wpfalse1())
         dispatch(wpfalse2())
-
     }
-
-
-
     return (
         <div className='z-40 flex flex-col justify-center items-center h-full w-full '>
             <div className='flex  justify-center items-center h-[93vh] w-full  max-lg:hidden'>
-
-
-
                 <div className={`gorsel bg-surg flex justify-center items-center h-full w-full duration-700 ${wpkay1?"bg-surgery1":""} ${wpkay2?"bg-surgery111":""} ${wpkay3?"bg-surgery222":""} bg-no-repeat bg-top max-md:bg-center `}>
                     {/*  */}
                     <div className='flex justify-center items-center h-full w-full mt-[2rem] '>
@@ -53,7 +40,6 @@ const MainBodyHead = () => {
                         <div className='flex justify-start items-start h-full w-full'>
                             <div className='flex justify-center items-center rounded-full h-[7rem] w-[7rem]  bg-kirmizidaire ml-[2rem]'></div>
                         </div>
-
                         <ul className='flex justify-around items-start h-full w-[200%] text-stone-700 font-bold'>
                             <li className='flex justify-center items-center h-[2rem] w-content hover:text-white duration-300 cursor-pointer'>ANASAYFA</li>
                             <li className='flex justify-center items-center h-[2rem] w-content hover:text-white duration-300 cursor-pointer'>HAKKIMIZDA</li>
@@ -61,7 +47,6 @@ const MainBodyHead = () => {
                             <li className='flex justify-center items-center h-[2rem] w-content hover:text-white duration-300 cursor-pointer'>EKİBİMİZ</li>
                             <li className='flex justify-center items-center h-[2rem] w-content hover:text-white duration-300 cursor-pointer'>BLOG</li>
                             <li className='flex justify-center items-center h-[2rem] w-content hover:text-white duration-300 cursor-pointer'>İLETİŞİM</li>
-
                         </ul>
                         <div className='flex justify-center items-start h-full w-full'>
                             <div className='flex justify-end items-start h-full w-[150%]'>
@@ -72,43 +57,23 @@ const MainBodyHead = () => {
                             </div>
                             <div className='flex justify-start items-start h-full w-[50%]'>
                                 <div className='flex justify-center items-center h-[1.5rem] w-[4rem] text-white bg-blue-400 hover:text-stone-700 hover:bg-transparent duration-300 cursor-pointer'>
-                                    <p className='text-[0.55rem] font-bold'>RANDEVU AL</p>
-
+                                    <h3 className='text-[0.55rem] font-bold'>RANDEVU AL</h3>
                                 </div>
                                 <ul className='flex flex-col justify-center items-center h-full w-content'>
                                     <li onClick={say1} className={`my-[0.5rem] cursor-pointer flex justify-center items-center w-[1rem] h-[1rem] rounded-full duration-500 bg-karelaci ${wpkay1 ? "border-[0.1rem] border-black" : ""}`}></li>
                                     <li onClick={say2} className={`my-[0.5rem] cursor-pointer flex justify-center items-center w-[1rem] h-[1rem] rounded-full duration-500 bg-karelaci ${wpkay2 ? "border-[0.1rem] border-black" : ""}`}></li>
                                     <li onClick={say3} className={`my-[0.5rem] cursor-pointer flex justify-center items-center w-[1rem] h-[1rem] rounded-full duration-500 bg-karelaci ${wpkay3 ? "border-[0.1rem] border-black" : ""}`}></li>
-
                                 </ul>
                             </div>
-
-
-
                         </div>
-
                     </div>
-
                 </div>
-
-
                 <div class=" custom-shape-divider-bottom-1699041665 ">
-
                     <svg className='' data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-
                         <path className='' d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z" class="shape-fill"></path>
-
                     </svg>
-
                 </div>
-
-
-
-
             </div>
-
-
-
         </div>
     )
 }
