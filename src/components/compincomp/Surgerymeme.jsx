cf906db5f5b6457278eda5d84e49b40275084ff4
Koray Afiyet <korayafiyet.bsns@gmail.com>
2023-11-06 03:45:18 +0300

    Update Surgerymeme.jsx

--- src/components/compincomp/Surgerymeme.jsx
+++ src/components/compincomp/Surgerymeme.jsx
@@ -3,13 +3,9 @@ import { FiChevronsRight } from 'react-icons/fi';
 const Surgerymeme = ({ sayi1, sayi2, sayi3, yol }) => {
     return (
         <div className='flex justify-center items-center h-full w-full '>
-
             <div className={`ani${yol} ml-[2rem] z-50 flex flex-col justify-center items-center h-[30rem] w-[20rem] bg-white rounded-md p-[1rem]`}>
-
                 <div className='flex justify-center items-center text-center h-[20rem] w-[18rem] '>
-
                     <img className='flex justify-center items-center h-full' src={`/images/mem${sayi1}.png`} alt="" />
-
                 </div>
                 <div className='flex flex-col justify-start items-center text-center h-[30%] w-full'>
                     <h5 className='font-semibold'>Meme Estetiği</h5>
@@ -20,11 +16,8 @@ const Surgerymeme = ({ sayi1, sayi2, sayi3, yol }) => {
                 </div>
             </div>
             <div className={`ani${yol} mx-[2rem] z-50 flex flex-col justify-center items-center h-[30rem] w-[20rem] bg-white rounded-md p-[1rem]`}>
-
                 <div className='flex justify-center items-center text-center h-[20rem] w-[18rem] '>
-
                     <img className='flex justify-center items-center h-full' src={`/images/mem${sayi2}.png`} alt="" />
-
                 </div>
                 <div className='flex flex-col justify-start items-center text-center h-[30%] w-full'>
                     <h5 className='font-semibold'>Meme Estetiği</h5>
@@ -35,11 +28,8 @@ const Surgerymeme = ({ sayi1, sayi2, sayi3, yol }) => {
                 </div>
             </div>
             <div className={`ani${yol} mr-[2rem] z-50 flex flex-col justify-center items-center h-[30rem] w-[20rem] bg-white rounded-md p-[1rem]`}>
-
                 <div className='flex justify-center items-center text-center h-[20rem] w-[18rem] '>
-
                     <img className='flex justify-center items-center h-full' src={`/images/mem${sayi3}.png`} alt="" />
-
                 </div>
                 <div className='flex flex-col justify-start items-center text-center h-[30%] w-full'>
                     <h5 className='font-semibold'>Meme Estetiği</h5>
